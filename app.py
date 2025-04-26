f1d873d883e93698e2b5adb415f7f8f2c1fd89b4
Omar Yajib <omaryajib@gmail.com>
2025-04-26 13:55:30 +0200

    fix hosting on render

--- app.py
+++ app.py
@@ -175,8 +175,9 @@ def export_csv():
 
 # Run app
 if __name__ == '__main__':
-    init_db()
     import os
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+    init_db()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
