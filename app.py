afb920eecfeedce8c0b66005c90b55bc40a11aff
Omar Yajib <omaryajib@gmail.com>
2025-04-26 13:50:31 +0200

    fix render port issue

--- app.py
+++ app.py
@@ -176,4 +176,7 @@ def export_csv():
 # Run app
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+
